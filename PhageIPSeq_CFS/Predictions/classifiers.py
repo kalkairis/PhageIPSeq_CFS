8edd90f684dccc7094ffa43c7a82ec1e46514f91
kalkairis <kalka.iris@gmail.com>
2021-08-08 22:24:41 +0300

    adding a summary of all sub groups of oligos

--- PhageIPSeq_CFS/Predictions/classifiers.py
+++ PhageIPSeq_CFS/Predictions/classifiers.py
@@ -1,5 +1,3 @@
-from sklearn.metrics import roc_curve, auc
-from numpy import interp
 import os
 
 import matplotlib.pyplot as plt
@@ -7,14 +5,14 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import shap
-from LabQueue.qp import qp, fakeqp
+from LabQueue.qp import qp
 from LabUtils.addloglevels import sethandlers
 from scipy.stats import pearsonr
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import RidgeClassifier
 from sklearn.metrics import auc
 
-from PhageIPSeq_CFS.config import repository_data_dir, visualizations_dir, logs_path, oligo_families
+from PhageIPSeq_CFS.config import visualizations_dir, logs_path, oligo_families
 from PhageIPSeq_CFS.helpers import get_oligos_with_outcome, split_xy_df_and_filter_by_threshold, \
     get_oligos_metadata_subgroup_with_outcome
 
@@ -33,7 +31,7 @@ def run_leave_one_out_prediction(x, y, predictor_class, *predictor_args, **predi
             predict_proba = predictor._predict_proba_lr(x.loc[sample].values.reshape(1, -1))[0][1]
         else:
             predict_proba = predictor.predict_proba(x.loc[sample].values.reshape(1, -1))[0][1]
-        ret[sample] = {'y': y.loc[sample].values[0], 'y_hat': y_hat, 'predict_proba': predict_proba}
+        ret[sample] = {'y': y.loc[sample], 'y_hat': y_hat, 'predict_proba': predict_proba}
     return pd.DataFrame(ret).transpose()
 
 
@@ -86,11 +84,12 @@ def get_x_y(bottom_threshold, data_type, oligos_subgroup):
     return x, y
 
 
-def get_prediction_parameters(data_type, threshold_percent, figures_dir, oligos_subgroup='all'):
+def get_prediction_parameters(data_type, threshold_percent, figures_dir, oligos_subgroup='all',
+                              predictor_class=RidgeClassifier, **predictor_kwargs):
     bottom_threshold = threshold_percent / 100
     x, y = get_x_y(bottom_threshold, data_type, oligos_subgroup)
     output_dir = os.path.join(figures_dir, data_type, str(bottom_threshold))
-    return get_prediction_results(output_dir, x, y, RidgeClassifier)
+    return get_prediction_results(output_dir, x, y, predictor_class, **predictor_kwargs)
 
 
 def get_top_prediction_shap_values(data_type, bottom_threshold, figures_dir, oligos_subgroup, predictor_class,
@@ -103,8 +102,16 @@ def get_top_prediction_shap_values(data_type, bottom_threshold, figures_dir, oli
     predictor = predictor_class(*predictor_args, **predictor_kwargs)
     model = predictor.fit(x, y)
 
-    explainer = shap.LinearExplainer(model, x)
-    shap_values_ebm = explainer(x, silent=True)
+    if isinstance(predictor, RidgeClassifier):
+        explainer = shap.LinearExplainer(model, x)
+        shap_values_ebm = explainer(x, silent=True)
+        pd.Series(index=x.columns, data=predictor.coef_[0]).sort_values(ascending=False).to_csv(
+            os.path.join(out_dir, 'feature_coefs.csv'))
+    else:
+        explainer = shap.TreeExplainer(model, x)
+        shap_values_ebm = explainer(x)
+        pd.Series(index=x.columns, data=predictor.feature_importances_).sort_values(ascending=False).to_csv(
+            os.path.join(out_dir, 'feature_coefs.csv'))
 
     # the waterfall_plot shows how we get from explainer.expected_value to model.predict(X)[sample_ind]
     ax = plt.subplot()
@@ -114,11 +121,8 @@ def get_top_prediction_shap_values(data_type, bottom_threshold, figures_dir, oli
     plt.savefig(os.path.join(out_dir, 'shap_beeswarm.png'))
     plt.close()
 
-    pd.Series(index=x.columns, data=predictor.coef_[0]).sort_values(ascending=False).to_csv(
-        os.path.join(out_dir, 'feature_coefs.csv'))
 
-
-def predict_and_run_shape_on_oligo_subgroup(oligos_subgroup, figures_dir):
+def predict_and_run_shape_on_oligo_subgroup(oligos_subgroup, figures_dir, predictore_class, **predictor_kwargs):
     curr_figures_dir = os.path.join(figures_dir, oligos_subgroup)
     with qp(f"preds") as q:
         q.startpermanentrun()
@@ -127,29 +131,34 @@ def predict_and_run_shape_on_oligo_subgroup(oligos_subgroup, figures_dir):
             for threshold_percent in range(0, 100, 5):
                 waiton[(data_type, threshold_percent)] = q.method(get_prediction_parameters,
                                                                   (data_type, threshold_percent, curr_figures_dir,
-                                                                   oligos_subgroup))
+                                                                   oligos_subgroup, predictore_class),
+                                                                  kwargs=predictor_kwargs)
         all_results = {k: q.waitforresult(v) for k, v in waiton.items()}
     all_results = pd.DataFrame(all_results).transpose().reset_index().rename(
-        columns={'level_0': 'data_type', 'level_1': 'bottom_threshold'})
+        columns={'level_0': 'data_type', 'level_1': 'bottom_threshold'}).sort_values(by='auc', ascending=False)
     all_results.to_csv(os.path.join(curr_figures_dir, 'results_summary.csv'))
     all_results.dropna(inplace=True)
-    best_params = all_results.sort_values(by='auc', ascending=False).iloc[0][
+    best_params = all_results.iloc[0][
         ['data_type', 'bottom_threshold']].to_dict()
-    get_top_prediction_shap_values(**best_params, predictor_class=RidgeClassifier, figures_dir=curr_figures_dir,
-                                   oligos_subgroup=oligos_subgroup)
+    get_top_prediction_shap_values(**best_params, predictor_class=predictore_class, figures_dir=curr_figures_dir,
+                                   oligos_subgroup=oligos_subgroup, **predictor_kwargs)
+    return all_results.iloc[0].to_dict()
 
 
 if __name__ == "__main__":
-    figures_dir = os.path.join(visualizations_dir, 'Predictions', 'RidgeClassifier')
+    predictor_class = GradientBoostingClassifier
+    predictor_kwargs = {"n_estimators": 2000, "learning_rate": .01, "max_depth": 6, "max_features": 1,
+                        "min_samples_leaf": 10}
+    figures_dir = os.path.join(visualizations_dir, 'Predictions', predictor_class.__name__)
     all_results = {}
     sethandlers()
     os.chdir(logs_path)
-    with fakeqp(f"wpreds") as q:
+    with qp(f"wpreds") as q:
         q.startpermanentrun()
-        waiton = []
+        waiton = {}
         for oligos_subgroup in oligo_families + ['all']:
-            waiton.append(q.method(predict_and_run_shape_on_oligo_subgroup, (oligos_subgroup, figures_dir)))
-        q.wait(waiton, assertnoerrors=False)
-    print("here")
-
-    print("here")
+            waiton[oligos_subgroup] = q.method(predict_and_run_shape_on_oligo_subgroup,
+                                               (oligos_subgroup, figures_dir, predictor_class), kwargs=predictor_kwargs)
+        all_results = {k: q.waitforresult(v) for k, v in waiton.items()}
+    pd.DataFrame(all_results).transpose().sort_values(by='auc', ascending=False).to_csv(
+        os.path.join(figures_dir, 'best_results_summary.csv'))
