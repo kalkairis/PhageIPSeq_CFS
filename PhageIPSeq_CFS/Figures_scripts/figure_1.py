2fbbf5eb5b507aa6f88e28fe597418d063cc3cd1
kalkairis <kalka.iris@gmail.com>
2022-01-03 16:54:55 +0200

    adding cohort info table which I don't think should appear there in the end.

--- PhageIPSeq_CFS/Figures_scripts/figure_1.py
+++ PhageIPSeq_CFS/Figures_scripts/figure_1.py
@@ -1,6 +1,6 @@
 import os
 import string
-
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -9,12 +9,12 @@ from skbio.diversity.alpha import shannon
 from statannot import add_stat_annotation
 from statsmodels.stats.multitest import multipletests
 
-from PhageIPSeq_CFS.config import visualizations_dir, oligo_families
+from PhageIPSeq_CFS.config import visualizations_dir, oligo_families, oligo_families_colors
 from PhageIPSeq_CFS.helpers import get_data_with_outcome, get_oligos_metadata, split_xy_df_and_filter_by_threshold, \
     get_individuals_metadata_df
 
 
-def create_figure_1(overwrite=True):
+def create_figure_1(with_table=False, overwrite=True):
     figures_dir = os.path.join(visualizations_dir, 'figure_1')
     os.makedirs(figures_dir, exist_ok=True)
     fig = plt.figure(figsize=(20, 10))
@@ -31,8 +31,67 @@ def create_figure_1(overwrite=True):
     ax = fig.add_subplot(spec[0, 1])
     ax.text(-0.1, 1.1, string.ascii_uppercase[1], transform=ax.transAxes, size=20, weight='bold')
     ax.set_axis_off()
-    internal_spec = spec[0, 1].subgridspec(1, 2, wspace=0)
+    internal_spec = spec[0, 1].subgridspec(2, 1, height_ratios=[1, 2])
     ax = fig.add_subplot(internal_spec[0])
+    ax.set_axis_off()
+    if with_table:
+        summary_table = cohort_info.copy()
+        summary_table['outcome'] = summary_table['catrecruit_Binary'].apply(
+            lambda x: 'CFS' if bool(x) else 'Healthy controls')
+        summary_groups = summary_table.groupby('outcome')
+        total_number = summary_groups.count().iloc[:, 0].to_frame()
+        total_number.columns = ['Total number']
+        total_number = total_number.stack()
+        total_number = total_number.to_frame().reset_index(-1).rename(columns={'level_1': 'level_2'})
+        sex_summary = summary_table.groupby(['outcome', 'Sex']).count().iloc[:, 0].to_frame()
+        sex_summary.columns = ['Sex']
+        sex_summary = sex_summary.stack()
+        sex_summary = pd.DataFrame({k: {'Male/Female': f'{v["Male"]}/{v["Female"]}'} for k, v in
+                                    sex_summary.reset_index(-1).drop(columns='level_2')[0].unstack(
+                                        -1).T.to_dict().items()}).T.stack()
+        sex_summary = sex_summary.to_frame().reset_index(-1)
+        sex_summary['level_2'] = 'Sex'
+        age_range_summary = summary_groups['agegroup_Average'].agg([min, max]).apply(
+            lambda row: f"{row['min']} to {row['max']}", axis=1)
+        age_averages = summary_groups['agegroup_Average'].agg(['mean', 'std']).round(1).apply(
+            lambda row: f"{row['mean']} +- {row['std']}", axis=1)
+        ages = pd.DataFrame({'Range': age_range_summary, r'Average +- SD': age_averages}).stack().to_frame()
+        ages['level_2'] = 'Age'
+        ages = ages.reset_index(-1)
+        summary_table = pd.concat([total_number, sex_summary, ages]).sort_index().reset_index()
+        table = ax.table(cellText=summary_table[[0]].astype(str).values,
+                         loc='center right', colWidths=[0.3], )
+        w = table.get_celld()[(0, 0)].get_width()
+        h = table.get_celld()[(0, 0)].get_height()
+        for i in range(2):
+            cell_1 = table.add_cell((i * 4) + 0, -1, w, h)
+            cell_1.visible_edges = "TBR"
+            cell_2 = table.add_cell((i * 4) + 0, -2, w, h)
+            cell_2.get_text().set_text('Total number')
+            cell_2.visible_edges = "TLB"
+            cell = table.add_cell((i * 4) + 1, -1, w, h)
+            cell.get_text().set_text('Male/Female')
+            cell = table.add_cell((i * 4) + 1, -2, w, h)
+            cell.get_text().set_text('Sex')
+            cell = table.add_cell((i * 4) + 2, -1, w, h)
+            cell.get_text().set_text('Range')
+            cell = table.add_cell((i * 4) + 3, -1, w, h)
+            cell.get_text().set_text(r"Average $\pm$ SD")
+            cell_1 = table.add_cell((i * 4) + 2, -2, w, h)
+            cell_1.visible_edges = "TRL"
+            cell_1.get_text().set_text('Age')
+            cell_2 = table.add_cell((i * 4) + 3, -2, w, h)
+            cell_2.visible_edges = "BRL"
+            cells = [table.add_cell((i * 4) + j, -3, w / 4, h) for j in range(4)]
+            cells[0].visible_edges = 'LTR'
+            cells[1].visible_edges = 'LR'
+            cells[2].visible_edges = 'LR'
+            cells[3].visible_edges = 'BLR'
+        ax.annotate('CFS', xy=(0.03, 0.7), rotation=90)
+        ax.annotate('Healthy', xy=(0.03, 0.005), rotation=90)
+
+    internal_figure_spec = internal_spec[1].subgridspec(2, 2, wspace=0, hspace=0.1, height_ratios=[5, 1])
+    ax = fig.add_subplot(internal_figure_spec[0, 0])
     sns.histplot(data=cohort_info[cohort_info['catrecruit_Binary'].eq(1)],
                  y='agegroup_Average', hue='Sex',
                  palette=[sns.color_palette("colorblind")[-4], sns.color_palette("colorblind")[-1]],
@@ -40,16 +99,23 @@ def create_figure_1(overwrite=True):
     ax.set_ylabel('Age group average')
     ax.set_xlabel('CFS')
     ax.set_xlim(*list(ax.get_xlim())[::-1])
-    ax.set_ylim(ax.get_ylim()[0], ax.get_ylim()[1] * 1.2)
-    ax = fig.add_subplot(internal_spec[1], sharey=ax)
+    ax.set_ylim(ax.get_ylim()[0], ax.get_ylim()[1])
+    ax = fig.add_subplot(internal_figure_spec[0, 1], sharey=ax)
     ax.get_yaxis().set_visible(False)
     ax = sns.histplot(data=cohort_info[cohort_info['catrecruit_Binary'].eq(0)], y='agegroup_Average',
                       bins=4,
                       palette=[sns.color_palette("colorblind")[-4], sns.color_palette("colorblind")[-1]],
                       hue='Sex',
-                      multiple="stack",
+                      multiple="stack", legend=False,
                       ax=ax)
+
     ax.set_xlabel('Healthy')
+    ax = fig.add_subplot(internal_figure_spec[1, :])
+    ax.set_axis_off()
+    ax.legend(
+        handles=[mpatches.Patch(facecolor=sns.color_palette("colorblind")[-4], label='Male', edgecolor='black'),
+                 mpatches.Patch(facecolor=sns.color_palette("colorblind")[-1], label='Female', edgecolor='black')],
+        loc='lower center', ncol=2)
 
     # sub-figure c
     ax = fig.add_subplot(spec[0, 2])
@@ -62,14 +128,14 @@ def create_figure_1(overwrite=True):
     df2['shannon_div'] = df2.apply(lambda row: shannon(row), axis=1)
     df2['status'] = df2.reset_index()['is_CFS'].astype(bool).apply(lambda x: 'CFS' if x else 'Healthy').values
 
-    internal_spec = spec[0, 2].subgridspec(1, 2, wspace=0)
-    ax = fig.add_subplot(internal_spec[0])
+    internal_figure_spec = spec[0, 2].subgridspec(1, 2, wspace=0)
+    ax = fig.add_subplot(internal_figure_spec[0])
     sns.boxplot(data=df, x='status', y='num_oligos', palette=sns.color_palette("muted")[-2:][::-1], ax=ax)
     ax.set_xlabel('')
     ax.spines['right'].set_linestyle('dashed')
     ax.set_ylabel('Number of oligos per individual')
 
-    ax = fig.add_subplot(internal_spec[1])
+    ax = fig.add_subplot(internal_figure_spec[1])
     ax.get_yaxis().set_visible(False)
     ax.set_frame_on(False)
     ax2 = ax.twinx()
@@ -90,7 +156,7 @@ def create_figure_1(overwrite=True):
     df.sort_values(by='is_flagellin', inplace=True)
     sns.scatterplot(data=df, x=cfs_label,
                     y=healthy_label, hue='is_flagellin',
-                    palette=['grey', sns.color_palette("Set2")[4]],
+                    palette=['lightgray', oligo_families_colors['bac flagella']],
                     ax=ax)
     legend_handels, legend_labels = ax.get_legend_handles_labels()
     legend_handels = [legend_handels[legend_labels.index('True')]]
@@ -112,7 +178,8 @@ def create_figure_1(overwrite=True):
     metadata[ratio_column] = df.apply(
         lambda oligo: oligo[int(True)] / oligo[int(False)])
     order = ['PNP', 'patho', 'probio', 'IgA', 'bac flagella', 'IEDB or cntrl']
-    ax = sns.boxplot(data=metadata, x='Oligo family', y=ratio_column, ax=ax, order=order)
+    ax = sns.boxplot(data=metadata, x='Oligo family', y=ratio_column, ax=ax, order=order,
+                     palette=list(map(lambda family: oligo_families_colors[family], order)))
     pval_res = {}
     for b1 in order:
         for b2 in order:
@@ -148,11 +215,12 @@ def create_figure_1(overwrite=True):
     ax.text(-0.1, 1.1, string.ascii_uppercase[5], transform=ax.transAxes, size=20, weight='bold')
 
     if overwrite:
-        plt.savefig(os.path.join(figures_dir, 'figure_1.png'))
+        plt.savefig(os.path.join(figures_dir, 'figure_1' + ('_with_table' if with_table else '') + '.png'))
     else:
         plt.show()
 
 
 if __name__ == "__main__":
-    overwrite = True
-    create_figure_1(overwrite)
+    overwrite = False
+    create_figure_1(with_table=True, overwrite=overwrite)
+    create_figure_1(with_table=False, overwrite=overwrite)
